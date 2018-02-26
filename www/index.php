89d1d6d9db95323266bf4dd5a152f0d1d77817a1
ADriano AVelino <adriano-st777@hotmail.com>
2018-02-26 09:41:16 -0300

    finaliza as rotas

--- www/index.php
+++ www/index.php
@@ -9,4 +9,53 @@ $app->get('/', function (Request $request, Response $response) use ($app) {
     return $response;
 });
 
+/**
+* Lista de livros
+**/
+$app->get('/book', function (Request $request, Response $response) use ($app)
+{
+    $response->getBody()->write("lista de livros");
+    return $response;
+});
+
+/**
+ * Retornando por id
+ */
+$app->get('/book/{id}', function (Request $request, Response $response) use ($app)
+{
+    $route = $request->getAttribute('route');
+    $id = $route->getArgument('id');
+    $response->getBody()->write("Exibindo o livro {$id}");
+});
+
+/**
+* cadastra um novo livro
+*/
+$app->post('/book/', function(Request $request, Response $response) use ($app)
+{
+    $response->getBody()->write("cadastrando um livro");
+    return $response;
+});
+
+/**
+* Atualiza os dados do livro
+*/
+$app->put('/book/{id}', function(Request $request, Response $response)
+{
+    $route = $request->getAttribute('route');
+    $id = $route->getArgument('id');
+    $response->getBody()->write("Atualizando o livro {$id}");
+});
+
+/**
+* Deleta o livro pelo id
+**/
+$app->delete('/book/{id}', function (Request $request, Response $response) use ($app)
+{
+    $route = $request->getAttribute('route');
+    $id = $route->getArgument('id');
+    $response->getBody()->write("Deleta o livro de id {$id}");
+});
+
+
 $app->run();
